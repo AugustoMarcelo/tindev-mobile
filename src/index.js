7177fa401bc070c5f082ac182d68fc3d017dd7e3
AugustoMarcelo <mrclgst10@gmail.com>
2019-08-08 19:42:00 -0300

    Importando tela de login

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Text } from 'react-native';
+
+import Login from './pages/Login';
 
 export default function App() {
-  return <Text>Hello World</Text>;
+  return <Login />;
 }
