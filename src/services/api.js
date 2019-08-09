3a0c3f92928c2a735001421a9d2576ea27aaa474
AugustoMarcelo <mrclgst10@gmail.com>
2019-08-08 21:51:33 -0300

    Modificando a url da API (adb reverse não funcionou)

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
   // adb reverse tcp:3333 tcp:3333 configurado
-  baseURL: 'http://localhost:3333',
+  baseURL: 'http://192.168.0.106:3333',
 });
 
 export default api;
